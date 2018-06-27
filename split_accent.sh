d4353a39a5d9b744a986f382424201a388cbdaea
Lee, Kui-Feng <kui.lee@booking.com>
2018-06-27 15:25:07 +0200

    Use ffmpeg to convert mp3 files to wav.

--- split_accent.sh
+++ split_accent.sh
@@ -4,9 +4,9 @@ for s in dev test train; do
     
     grep indian cv-valid-$s.csv > cv-valid-$s-indian.csv
     mkdir data/$s/indian
-    cut -d',' -f1 cv-valid-$s-indian.csv | xargs -I'{}' ln -s ${PWD}/{} data/$s/indian
+    cut -d',' -f1 cv-valid-$s-indian.csv | xargs -I'{}' sh -c 'ffmpeg -i {} data/'$s'/indian/$(basename {}).wav'
 
     grep ',us,' cv-valid-$s.csv > cv-valid-$s-us.csv
     mkdir data/$s/us
-    cut -d',' -f1 cv-valid-$s-us.csv | xargs -I'{}' ln -s ${PWD}/{} data/$s/us
+    cut -d',' -f1 cv-valid-$s-us.csv | xargs -I'{}' sh -c 'ffmpeg -i {} data/'$s'/us/$(basename {})'.wav
 done
