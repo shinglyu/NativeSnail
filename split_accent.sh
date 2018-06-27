06cccf25bd8606ac34266d91362f92b45c156d28
Lee, Kui-Feng <kui.lee@booking.com>
2018-06-27 14:49:19 +0200

    Make datasets with symbolic links.

--- split_accent.sh
+++ split_accent.sh
@@ -1,10 +1,12 @@
-grep indian cv-valid-dev.csv > cv-valid-dev-indian.csv
-grep indian cv-valid-train.csv >> cv-valid-dev-indian.csv
-mkdir cv-valid-dev-indian
-cut -d',' -f1 cv-valid-dev-indian.csv | xargs -I'{}' cp {} cv-valid-dev-indian
+mkdir data
+for s in dev test train; do
+    mkdir data/$s
+    
+    grep indian cv-valid-$s.csv > cv-valid-$s-indian.csv
+    mkdir data/$s/indian
+    cut -d',' -f1 cv-valid-$s-indian.csv | xargs -I'{}' ln -s ${PWD}/{} data/$s/indian
 
-
-grep ',us,' cv-valid-dev.csv > cv-valid-dev-us.csv
-grep ',us,' cv-valid-train.csv >> cv-valid-dev-us.csv
-mkdir cv-valid-dev-us
-cut -d',' -f1 cv-valid-dev-us.csv | xargs -I'{}' cp {} cv-valid-dev-us
+    grep ',us,' cv-valid-$s.csv > cv-valid-$s-us.csv
+    mkdir data/$s/us
+    cut -d',' -f1 cv-valid-$s-us.csv | xargs -I'{}' ln -s ${PWD}/{} data/$s/us
+done
